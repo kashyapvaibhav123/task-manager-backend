dfe754a0653b5d61cd968aeed7a09eb81d757f49
kashyapvaibhav123 <kashyapvaibhav1244@gmail.com>
2026-02-08 22:53:42 +0530

    changes in setttings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,10 +14,11 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-secret-key")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "localhost,127.0.0.1,.onrender.com"
-).split(",")
+ALLOWED_HOSTS = [
+    "task-manager-backend-3use.onrender.com",  # your Render app URL
+    "127.0.0.1",  # optional for local testing
+    "localhost",  # optional for local testing
+]
 
 
 # ======================
